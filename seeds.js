7f0956961cf2820dc16d80e338f54fa87fcdf5da
Sulaiman Alhabib <suldev8@gmail.com>
2019-09-07 16:50:40 +0300

    Only seed categories

--- seeds.js
+++ seeds.js
@@ -13,81 +13,17 @@ mongoose.connect(db, {
   useMongoClient: true
 });
 
+connection = mongoose.connection;
+
 const categories = ['Abuse', 'Cancer', 'Marriage'];
-
-const dummyUser = {
-  username: 'dummy',
-  password: '123123123'
-};
-
-
 
 categories.forEach((category, index) => {
   const newCategory = new Category({
     name: category
   });
 
-  newCategory.save();
+  newCategory.save().then(category => {
+    console.log(`Added ${category.name}`);
+    connection.close();
+  });
 });
-
-// const dummyUserCreated = Promise.resolve(dummyUser)
-//   .then(credentials => {
-//     if (
-//       !credentials ||
-//       !credentials.password ||
-//       credentials.password !== credentials.password_confirmation
-//     ) {
-//       throw new BadParamsError();
-//     }
-//   })
-//   // generate a hash from the provided password, returning a promise
-//   .then(() => bcryptjs.hash(req.body.credentials.password, bcryptSaltRounds))
-//   .then(hash => {
-//     // return necessary params to create a user
-//     return {
-//       username: req.body.credentials.username,
-//       hashedPassword: hash
-//     };
-//   })
-//   // create user with provided username and hashed password
-//   .then(user => User.create(user).then(user => {
-      
-//   })
-//   )
-
-
-// console.log("dummyID:", dummyUserCreated.id);
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// Category.find({}).then(categories => {
-//     categories.forEach(value => value.remove())
-//     db.close();
-// })
-// Post.find({}).then(Document => {
-//     posts.forEach(value => user.remove())
-//     db.close();
-// })
-// User.find({}).then(users => {
-//     users.forEach(value => user.remove())
-//     db.close();
-// })
-
-// User.find({}).then(users => {
-//     users.forEach(value => user.remove())
-//     db.close();
-// })
